38d3b47d6eb9a2344bb2d63882723c370fbf6869
Giorgio Masci <gmasci@collaboration.codearchitects.net>
2016-06-09 11:35:22 +0200

    Raw Commit"

---
+++ Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CalculatorKata
+{
+	internal class Calculator
+	{
+		public Calculator()
+		{
+		}
+
+		public int Add(string v)
+		{
+			if (string.IsNullOrEmpty(v))
+				return 0;
+			var retval = 0;
+			string[] numbers = v.Split(',');
+			foreach (string number in numbers)
+				retval += Int32.Parse(number);
+			return retval;
+		}
+	}
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -6,9 +6,35 @@ namespace CalculatorKata
 	[TestClass]
 	public class UnitTest1
 	{
+		Calculator _calc = new Calculator();
+
 		[TestMethod]
-		public void TestMethod1()
+		public void StringEmptyReturnZero()
 		{
+			AssertSum(0, "");
+		}
+
+		[TestMethod]
+		public void StringReturnNumber()
+		{
+			AssertSum(1, "1");
+		}
+
+		[TestMethod]
+		public void StringSequenceReturnSum()
+		{
+			AssertSum(3, "1,2");
+		}
+
+		[TestMethod]
+		public void StringSequence2ReturnSum()
+		{
+			AssertSum(6, "1,2,3");
+		}
+
+		private void AssertSum(int expected, string actual)
+		{
+			Assert.AreEqual(expected, _calc.Add(actual));
 		}
 	}
 }
